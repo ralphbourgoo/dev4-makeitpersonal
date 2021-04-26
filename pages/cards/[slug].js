ab6afb63947433f28cd7def41fb7ec4d0822ec62
Ralph Bourgoo <ralph.bourgoo@student.howest.be>
2021-04-26 12:23:41 +0200

    Card CSS Complete

--- pages/cards/[slug].js
+++ pages/cards/[slug].js
@@ -1,6 +1,8 @@
 import { createClient } from 'contentful'
 import Image from 'next/image'
 import React from 'react'
+import styles from './Card.module.css'
+import Link from 'next/link'
 
 const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
@@ -38,27 +40,38 @@ export async function getStaticProps({ params }) {
 
 export default function CardDetails({ card }) {
     console.log(card)
-    // const { title, slug, receiver, sender, message, image } = card.fields
     const { image, title, receiver, sender, message} = card.fields
 
     return (
         <div>
-            <div className="card">
-                <div>
+            <div className={styles.card}>
+                <div className={styles.card_image}>
                     <Image
                         src={'https:' + image.fields.file.url}
                         width={image.fields.file.details.image.width}
                         height={image.fields.file.details.image.height}
                     />
                 </div>
-                <div>
-                    <h2>{title}</h2>
+                <div className={styles.card_content}>
+                    <h2 className={styles.card_title}>{title}</h2>
                     <div>
-                        <p>{receiver}</p>
-                        <p>{message}</p>
-                        <p>{sender}</p>
+                        <p className={styles.card_receiver}>{receiver}</p>
+                        <p className={styles.card_message}>{message}</p>
+                        <p className={styles.card_sender}> - {sender}</p>
+                    </div>
+                    <div className={styles.card_heart}>
+                        <Image
+                            src='/heart.svg'
+                            alt= 'Love Mom Heart'
+                            width='150'
+                            height='150'
+                        />
                     </div>
                 </div>
+            </div>
+            <div>
+                <h2>Share this link to send your mother the card:</h2>
+                <p>https://leho-howest.instructure.com/courses/9545/assignments/68575</p>
             </div>
         </div>
     )
