ed900a31524d1d570e333bb1fee175b9fb783078
Ralph Bourgoo <ralph.bourgoo@student.howest.be>
2021-04-27 09:08:16 +0200

    HTML & CSS Homepage

--- components/Layout.js
+++ components/Layout.js
@@ -14,10 +14,10 @@ export default function Layout({ children }) {
                     <Link href="/"><a className={styles.logo}>Mothersday  Card</a></Link>
                 </div>
                 <div>
-                    <Link href="/"><a>Home</a></Link>
-                    <Link href="/about"><a>About</a></Link>
-                    <Link href="/cards"><a>All Cards</a></Link>
-                    <Link href="/form"><a>Send a card</a></Link>
+                    <Link href="/"><a className={styles.navlink}>Home</a></Link>
+                    <Link href="/about"><a className={styles.navlink}>About</a></Link>
+                    <Link href="/cards"><a className={styles.navlink}>All Cards</a></Link>
+                    <Link href="/form"><a className={styles.navbutton}>>Send a card</a></Link>
                 </div>
             </header>
 
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-
-      <main className={styles.main}>
-        
-      </main>
-
-
+    <div className={styles.home_container}>
+      <div className={styles.home_header}>
+        <h1 className={styles.home_title}>Happy Mothersday !</h1>
+        <p className={styles.home_sub}>Send a motherdays card to you mother to wish her the best motherday !</p>
+      </div>
+      <div className={styles.home_img}>
+        <Image
+          src='/momcard.jpg'
+          alt='Mothersday Card'
+          width='1920'
+          height='1080'
+        />
+      </div>
+      <div>
+        <Link href="/form"><a className={styles.home_button}>Send a card</a></Link>
+      </div>
     </div>
   )
 }
