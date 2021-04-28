85d583179fd6566245fa94d575d18d71f814c82c
Ralph Bourgoo <ralph.bourgoo@student.howest.be>
2021-04-28 08:13:20 +0200

    Conditional Redirect

--- pages/cards/[slug].js
+++ pages/cards/[slug].js
@@ -34,6 +34,15 @@ export async function getStaticProps({ params }) {
         'fields.slug': params.slug
     })
 
+    if (!items.length) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
     return {
         props: { card: items[0] },
         revalidate: 5
