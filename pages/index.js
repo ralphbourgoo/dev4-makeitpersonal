faaed68a4b0790190a071b2153c3c1df75ea4cd8
Ralph Bourgoo <ralph.bourgoo@student.howest.be>
2021-05-02 11:32:21 +0200

    Responsive Home Page

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,9 @@ export default function Home() {
         <h1 className={styles.home_title}>Happy Mothersday ! </h1>
         <p className={styles.home_sub}>Send a motherdays card to your mother, to wish her the best motherday !</p>
         <p className={styles.text}>Looking for a unique and personalised motherday present? Then this can be something for you. Create a personalised motherday card. You can send this unique link to your mother and surprise her with a lovely message.  </p>
-        <div>
+        <div className={styles.button_wrapper}>
           <Link href="/form"><a className={styles.home_button}>Send a card</a></Link>
+          <Link href="/form"><a className={styles.home2_button}>Learn More</a></Link>
         </div>
       </div>
       <div className={styles.home_img}>
